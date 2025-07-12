984c1d5e8b6b408e2330dd79e06d56345c5eac26
Fernanda Cruz <63127943+Feracrz@users.noreply.github.com>
2025-07-12 12:02:57 -0600

    funcionalida de catalogos manera local

--- src/components/registroCompra/Registro.jsx
+++ src/components/registroCompra/Registro.jsx
@@ -13,6 +13,75 @@ import { parseStringPromise } from 'xml2js';
 import { XMLParser } from 'fast-xml-parser';
 
 const initialData = [];
+const recursosPorCategoria = {
+  "Papelería": [
+    "Acuarela escolar", "Bicolor", "Block para acuarela", "Borradores de pizarrón blanco",
+    "Carpeta blanca carta tres orificios arillo en 3 pulgadas", "Carpeta blanca carta tres orificios arillo en 4 pulgadas",
+    "Carpeta blanca carta tres orificios arillo en 5 pulgadas", "Carpeta 2 orificios tamaño carta", "Carpeta 2 orificios tamaño oficio",
+    "Charola organizador", "Chinchetas", "Cinta adhesiva transparente delgada", "Cinta canela",
+    "Clip mariposa", "Clip No. 2", "Correcto lápiz", "Corrector en cinta", "Cuaderno profesional",
+    "Cúter", "Dedal", "Diurex", "Diurex 1 pulgada", "Engrapadora", "Etiquetas no.24", "Etiquetas no.25",
+    "Ficha bibliográfica", "Ficha de trabajo", "Folder tamaño carta", "Folder tamaño oficio", "Gaveta del no 14",
+    "Gaveta paleta", "Goma", "Grapas", "Lapicero tinta azul", "Lapicero tinta azul de gel", "Lapicero tinta negra",
+    "Lapicero tinta rojo", "Lápiz", "Lápiz adhesivo", "Lápiz carmín rojo", "Libreta forma francesa pasta dura",
+    "Ligas #18", "Marcador permanente negro", "Marcador permanente rojo", "Marca texto", "Notas adhesivas chico",
+    "Notas adhesivas grande", "Notas adhesivas medianas", "Papel bond tamaño carta", "Papel bond tamaño carta colores intensos",
+    "Papel bond tamaño oficio", "Papel opalina blanca t/oficio", "Papel opalina tamaña carta 125 gr",
+    "Papel opalina tamaña carta 225 gr", "Perforadora de 1 orificio", "Perforadora de 2 orificios",
+    "Perforadora de 3 orificios", "Plumón de pizarrón", "Post-it-banderitas", "Protector de hoja carta",
+    "Quita grapas", "Regla metálica 30cm", "Sacapuntas", "Sello de acuse", "Separadores de hojas 10 divisiones",
+    "Separadores de hojas 15 divisiones", "Sobre carta", "Sobre de papel (CD/DVD)", "Sobre oficio",
+    "Sujeta documentos chico 25mm", "Sujeta documentos grande 51mm", "Sujeta documentos mediano 32mm",
+    "Tijera"
+  ],
+  "Material de limpieza": [
+    "Aromatizante en aerosol", "Bolsa negra grande", "Bolsa negra mediana", "Bolsa para cesto 61x61",
+    "Cepillo de acero inoxidable", "Cloro", "Cloro para mascotas", "Desinfectante en aerosol mediano",
+    "Detergente liquido", "Escoba", "Fabulo", "Fibra", "Gel antibacterial", "Guantes de látex",
+    "Insecticida", "Jabón de cartucho", "Jabón en polvo", "Jabón para manos", "Jerga gruesa", "Lija de agua",
+    "Limpiador para piso laminados", "Líquido para vidrios", "Lustrador de muebles en aerosol", "Mechudo",
+    "Microfibras", "Mop Americano", "Pastilla desodorante", "Pino", "Sanitas"
+  ],
+  "Insumos de oficina": [
+    "Agua embotellada",
+    "Azúcar refinada",
+    "Bebida rehidratante suero",
+    "Café soluble",
+    "Charolas desechables",
+    "Cuchara cafetería",
+    "Cuchara grande",
+    "Galletas finas",
+    "Galletas surtido rico",
+    "Kleenex",
+    "Papel higiénico",
+    "Papel higiénico jumbo",
+    "Pila 9V",
+    "Pila AA",
+    "Pila AAA",
+    "Pila botón modelo CR2032",
+    "Refresco de lata coca cola",
+    "Refresco de lata sabores",
+    "Servilletas",
+    "Te",
+    "Tenedor biodegradable desechable",
+    "Toalla desinfectantes",
+    "Vasos térmicos"
+  ],
+
+  "Consumibles": [
+  "DVD-R", "Memoria USB 16gb", "Memoria USB 32gb", "Memoria USB 64gb", "Memoria USB 128gb",
+  "Ribbon idp", "Ribon holograma", "Tambor de imagen DR820", "Tambor de imagen HP 219ª",
+  "Tinta color negra", "Tinta HP 954 BK", "Tinta HP 954 C", "Tinta HP 954 M", "Tinta HP 954 Y",
+  "Tinta HP GT52 Black", "Tinta HP GT52 Cian", "Tinta HP GT52 Magenta", "Tinta HP GT52 Yellow",
+  "Tóner 410A negro", "Tóner 5272 negro", "Tóner CE 285A", "Tóner CE 310A", "Tóner CE 311A",
+  "Tóner CE 312A amarillo", "Tóner CE 313A magenta", "Tóner HP CF280A", "Tóner HP 136A", 
+  "Tóner HP 410", "Tóner HP 411", "Tóner HP 412", "Tóner HP 413", "Tóner HP 414C", "Tóner HP 414B", 
+  "Tóner HP 414M", "Tóner HP 414Y", "Tóner HP CF278A", "Tóner TK 1122", "Tóner TK 1147", 
+  "Tóner TK 1152", "Tóner TK 3102", "Tóner TK 3182", "Tóner TK 5272C", "Tóner TK 5272K", 
+  "Tóner TK 5272M", "Tóner TK 5272Y", "Tóner TK 6307", "Tóner TN 1060", "Tóner TN 850", 
+  "Tóner TN890", "Tóner W1360A", "Tóner W2020A", "Tóner W2021A", "Tóner W2023A"
+]
+};
 
 const Registro = () => {
   const [data, setData] = useState(initialData);
@@ -241,7 +310,7 @@ const handleXMLUpload = async (e) => {
         </Col>
       </Row>
 
-      {/* Aquí puedes conservar tu tabla y modales previos como ya estaban */}
+      {/* Aquí tu tabla y modales previos como ya estaban */}
 
       {/* Modal de Factura */}
       <Modal size="lg" show={showFacturaModal} onHide={() => setShowFacturaModal(false)}>
@@ -292,18 +361,21 @@ const handleXMLUpload = async (e) => {
         </Form.Select>
       </td>
       <td> <Form.Select
-          value={item.recurso || ''}
-          onChange={e => {
-            const copy = [...facturaRows];
-            copy[idx].categoria = e.target.value;
-            setFacturaRows(copy);
-          }}
-        >
-          <option value="">Seleccione...</option>
-          <option value="Papelería">Papelería</option>
-          <option value="Material de limpieza">Material de limpieza</option>
-          <option value="Insumos">Insumos</option>
-        </Form.Select></td>
+  value={item.recurso || ''}
+  onChange={e => {
+    const copy = [...facturaRows];
+    copy[idx].recurso = e.target.value;
+    setFacturaRows(copy);
+  }}
+>
+  <option value="">Seleccione...</option>
+  {(recursosPorCategoria[item.categoria] || []).map((recurso, i) => (
+    <option key={i} value={recurso}>{recurso}</option>
+  ))}
+</Form.Select>
+        
+        
+        </td>
 
 
       
